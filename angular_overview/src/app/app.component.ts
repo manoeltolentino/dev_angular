ae70bfe52ca1910b8a5eacf0d06f29ece664241b
manoeltolentino <manoeltolentino@outlook.com>
2025-01-16 20:12:19 -0300

    commit

--- angular_overview/src/app/app.component.ts
+++ angular_overview/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import {Component} from '@angular/core';
-import { HomeComponents } from './home/home.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [HomeComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'default';
