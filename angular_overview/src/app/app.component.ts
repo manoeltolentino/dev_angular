e8586138f26e5b70c1be5ac4b173b6d8c40b07b3
manoeltolentino <manoeltolentino@outlook.com>
2025-01-22 08:22:01 -0300

    commit

--- angular_overview/src/app/app.component.ts
+++ angular_overview/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent],
+  imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
