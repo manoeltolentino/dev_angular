572959a3922c81aa59fa260e432703ca88e6d9ed
manoeltolentino <manoeltolentino@outlook.com>
2025-01-23 21:42:35 -0300

    Update app.component.ts

--- angular_overview/src/app/app.component.ts
+++ angular_overview/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import {Component} from '@angular/core';
 import { HomeComponent } from './home/home.component';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import routeconfig from './routes';
 
 @Component({
   selector: 'app-root',
   imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  providers: [ {provide: ActivatedRoute, useValue: routeconfig}]
 })
 export class AppComponent {
   title = 'default';
