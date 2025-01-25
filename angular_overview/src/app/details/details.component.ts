5e376429ea8b212f84e9efb28dd47c9cdeea897b
manoeltolentino <manoeltolentino@outlook.com>
2025-01-25 10:37:47 -0300

    Complete tutorial

--- angular_overview/src/app/details/details.component.ts
+++ angular_overview/src/app/details/details.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { Housinglocation } from '../housinglocation';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss'
 })
 export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService = inject(HousingService);
+  housingLocation: Housinglocation | undefined;
+  applyForm = new FormGroup({
+    firstName: new FormGroup(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
+
+  constructor(){
+
+    const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
+    
+    this.housingService.getHousingLocationById(housingLocationId).then((housingLocation) => {
+      this.housingLocation = housingLocation;
+    })
+
+  }
+
+  submitApplication(){
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    );
+
+
+
+  }
 
 }
--- angular_overview/src/app/housing-location/housing-location.component.ts
+++ angular_overview/src/app/housing-location/housing-location.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Housinglocation } from '../housinglocation';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-housing-location',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.scss'
 })
