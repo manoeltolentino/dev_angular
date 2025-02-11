4ea57f19d60dd3b1178c65e3798d9e69d6e5f87a
manoeltolentino <manoeltolentino@outlook.com>
2025-02-11 17:18:51 -0300

    Commit

--- teste-angular-side-menu/src/app/app.routes.ts
+++ teste-angular-side-menu/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {path: 'analytics', component: SidenavComponent},
+    {path: 'analytics', component: HomeComponent},
     {path: '', component: AppComponent}
 ];
